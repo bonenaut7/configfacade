bc1947228068edbbcced169a3aa1450d6a514181
Matvey Zholudz <fili.kabban@gmail.com>
2025-09-09 16:11:26 +0300

    Add isEmpty() to ConfigArray

--- src-core/src/main/java/by/bonenaut7/configfacade/facade/elements/ConfigArray.java
+++ src-core/src/main/java/by/bonenaut7/configfacade/facade/elements/ConfigArray.java
@@ -12,52 +12,45 @@ public final class ConfigArray extends ConfigElement implements Iterable<ConfigE
 		return this;
 	}
 	
-	public boolean containsArrays() {
-		for (int index = 0; index < this.list.size(); index++) {
-			if (this.list.get(index) instanceof ConfigArray) {
-				return true;
-			}
-		}
-		return false;
+	public int size() {
+		return this.list.size();
 	}
-
-	public boolean containsCompounds() {
-		for (int index = 0; index < this.list.size(); index++) {
-			if (this.list.get(index) instanceof ConfigCompound) {
-				return true;
-			}
-		}
-		return false;
+	
+	public boolean isEmpty() {
+		return this.list.isEmpty();
 	}
 
 	public ConfigElement getAt(int index) {
 		if (index > -1 && index < this.list.size()) {
 			return this.list.get(index);
 		}
+		
 		return null;
 	}
 
 	public ConfigArray getArrayAt(int index) {
 		if (index > -1 && index < this.list.size()) {
-			ConfigElement wrapper = this.list.get(index);
-			return wrapper instanceof ConfigArray ? (ConfigArray)wrapper : null;
+			final ConfigElement element = this.list.get(index);
+			return element instanceof ConfigArray ? (ConfigArray)element : null;
 		}
+		
 		return null;
 	}
 
 	public ConfigCompound getCompoundAt(int index) {
 		if (index > -1 && index < this.list.size()) {
-			ConfigElement wrapper = this.list.get(index);
-			return wrapper instanceof ConfigCompound ? (ConfigCompound)wrapper : null;
+			final ConfigElement element = this.list.get(index);
+			return element instanceof ConfigCompound ? (ConfigCompound)element : null;
 		}
 		return null;
 	}
 
 	public ConfigPrimitive getPrimitiveAt(int index) {
 		if (index > -1 && index < this.list.size()) {
-			ConfigElement wrapper = this.list.get(index);
-			return wrapper instanceof ConfigPrimitive ? (ConfigPrimitive)wrapper : null;
+			final ConfigElement element = this.list.get(index);
+			return element instanceof ConfigPrimitive ? (ConfigPrimitive)element : null;
 		}
+		
 		return null;
 	}
 
@@ -113,8 +106,22 @@ public final class ConfigArray extends ConfigElement implements Iterable<ConfigE
 		return this.list.remove(size() - 1);
 	}
 	
-	public int size() {
-		return this.list.size();
+	public boolean containsArrays() {
+		for (int index = 0; index < this.list.size(); index++) {
+			if (this.list.get(index) instanceof ConfigArray) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean containsCompounds() {
+		for (int index = 0; index < this.list.size(); index++) {
+			if (this.list.get(index) instanceof ConfigCompound) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	@Override
